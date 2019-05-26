6df473afab9414300503de7c952f7a9bc2941174
Kevin PINTO <kpinto@vente-privee.com>
2019-05-26 20:45:59 +0200

    feat(home): update ui + content

--- src/App/Main/Home/index.jsx
+++ src/App/Main/Home/index.jsx
@@ -1,10 +1,28 @@
+// @flow
 import React from 'react'
+import withConnect from './connector'
 import style from './style'
 
-const Home = () => (
+type Props = {|
+  +device: Device,
+  +onGetDevice: () => void
+|}
+
+const Home = ({ device, onGetDevice }: Props) => (
   <div className={style.wrapper}>
-    <h2>Welcome in MyLittleApp</h2>
+    <h1>Welcome in MyLittleApp</h1>
+
+    <h3>Basic side effect example</h3>
+    <p>Getting device infos from within EPIC middleware and setting result in application state</p>
+    <button onClick={onGetDevice} type='button'>
+      Get Device info
+    </button>
+
+    <h3>
+      <strong>Current Device :</strong>
+      <pre>{JSON.stringify(device, null, 2) }</pre>
+    </h3>
   </div>
 )
 
-export default Home
+export default withConnect(Home)
