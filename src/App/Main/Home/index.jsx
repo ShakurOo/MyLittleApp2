dff7d68633854e70972079c37214a6da146b333a
Kevin PINTO <kpinto@vente-privee.com>
2019-05-26 21:20:14 +0200

    feat(Home): update text content

--- src/App/Main/Home/index.jsx
+++ src/App/Main/Home/index.jsx
@@ -12,8 +12,16 @@ const Home = ({ device, onGetDevice }: Props) => (
   <div className={style.wrapper}>
     <h1>Welcome in MyLittleApp</h1>
 
-    <h3>Basic side effect example</h3>
-    <p>Getting device infos from within EPIC middleware and setting result in application state</p>
+    <h3>Basic data flow example</h3>
+    <p>Explanation :</p>
+    <ul>
+      <li>Button component trigger an event (redux action)</li>
+      <li>The action is handled by a middleware (Epic)</li>
+      <li>The middleware produce a side-effect as trigger a new action including in payload the correct device data</li>
+      <li>A reducer catch this action and populate the store</li>
+      <li>A selector (getter), detects the change, compute the newest value</li>
+      <li>Finally, the prop is updated, the connected component is rerender through the inner component lifecycle mechanisme</li>
+    </ul>
     <button onClick={onGetDevice} type='button'>
       Get Device info
     </button>
@@ -22,6 +30,7 @@ const Home = ({ device, onGetDevice }: Props) => (
       <strong>Current Device :</strong>
       <pre>{JSON.stringify(device, null, 2) }</pre>
     </h3>
+    <p><strong>Check the logs in the Console (F12)</strong></p>.
   </div>
 )
 
