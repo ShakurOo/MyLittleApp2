9fa131c6ee62da074673fe8a20f6d97ec22efb40
Kevin <kpinto.web@gmail.com>
2020-08-04 19:40:36 +0200

    refactor(webpack): reduce webpack configuration to minimum

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
+const precss = require('precss')
 
 module.exports = {
   entry: './src/index.tsx',
@@ -9,11 +10,38 @@ module.exports = {
   },
   devtool: 'source-map',
   module: {
-    rules: [{ 
-      exclude: /node_modules/,
-      test: /\.ts(x?)$/, 
-      loader: 'ts-loader'
-    }]
+    rules: [
+      { 
+        exclude: /node_modules/,
+        test: /\.ts(x?)$/, 
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|webp|svg)$/i,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            ident: 'postcss',
+            options: {
+              sourceMap: true,
+              plugins: () => [precss()]
+            }
+          }
+        ]
+      }
+    ]
   },
   output: {
     path: path.join(__dirname, '/dist'),
@@ -28,6 +56,6 @@ module.exports = {
     alias: {
       '@app': path.resolve(__dirname, 'src')
     },
-    extensions: ['.ts', '.tsx', '.js', '.json']
+    extensions: ['.css', '.ts', '.tsx', '.js', '.json']
   }
 }
